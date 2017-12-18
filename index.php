b010368012dbfce6dddbcf93359850791ff09d9e
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2017-12-18 23:35:15 +0530

    making ajax call for login

--- index.php
+++ index.php
@@ -45,7 +45,8 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     <script type="text/javascript">
@@ -70,6 +71,24 @@
           SignUp=0;
         }
       });
+
+      $("#login").click(() => {
+        $.ajax({
+          type:"post",
+          url:"actions.php?actions=login",
+          data:"username="+$("#InputUsername").val()+"&password="+$("#InputPassword").val(),
+          success: function(result){
+            if(result == "1"){
+              console.log("success");
+            }
+            else{
+              console.log("failure");
+            }
+          }
+
+        });
+      });
+      
     </script>
   </body>
 </html>
