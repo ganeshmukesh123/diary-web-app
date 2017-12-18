6ad2f1275c2adcc7a405c0471e7b266bfb4bef98
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2017-12-19 00:20:10 +0530

    adding database file

---
+++ db/dairy.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 18, 2017 at 07:49 PM
+-- Server version: 10.1.22-MariaDB
+-- PHP Version: 7.1.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dairy`
+--
+CREATE DATABASE IF NOT EXISTS `dairy` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `dairy`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diary_users`
+--
+
+CREATE TABLE `diary_users` (
+  `user_id` int(3) NOT NULL,
+  `username` text NOT NULL,
+  `email` text NOT NULL,
+  `content` text NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `diary_users`
+--
+ALTER TABLE `diary_users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `diary_users`
+--
+ALTER TABLE `diary_users`
+  MODIFY `user_id` int(3) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
