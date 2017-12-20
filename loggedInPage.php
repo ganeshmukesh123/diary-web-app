5be293f301ee26a48c6ce2406d20482f938d223e
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2017-12-21 01:39:02 +0530

    displaying content from database

--- loggedInPage.php
+++ loggedInPage.php
@@ -14,6 +14,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body>
   	<nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -36,11 +37,28 @@
 
 	  </div>
 	</nav>
+	<div class="form-group" style="margin:16px;
+	">
+	    <textarea class="form-control" id="diary_content" rows="16">
+	       <?php
+		   $query="SELECT `content` FROM `diary_users` WHERE `user_id`='".$_SESSION['id']."'";
+		   $result=mysqli_query($conn,$query);
+		   $row=mysqli_fetch_assoc($result);
+		   echo $row['content']; ?>
+		   	
+		</textarea>
+	</div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+    	$("#diary_content").bind('input propertychange',function(){
+    		console.log($("#diary_content").val());
+    	});
+    </script>
   </body>
 </html>
